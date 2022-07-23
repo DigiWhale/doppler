74776a4f9fbd1e59b5dadffc92badb1ad88a90f7
digiwhale <digiwhale.humphrey@gmail.com>
2022-07-23 19:05:30 -0400

    1

--- doppler_class.py
+++ doppler_class.py
@@ -49,7 +49,7 @@ class Doppler:
   def getSpeed(self) -> float:
     data = self.ser.readline()
     print(len(data))
-    print(data)
+    print(data.decode('utf-8'))
                 
 while __name__ == '__main__':
   doppler = Doppler()
